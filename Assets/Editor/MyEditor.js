6ec47e5fb2cb776f34938c82e5860ee651a02f8c
Aaron Calero <aaroncalero1@yahoo.es>
2014-02-25 19:09:12 +0100

    añadido script de editor para jenkins

---
+++ Assets/Editor/MyEditor.js
@@ -0,0 +1,28 @@
+class MyEditorScript {
+        static string[] SCENES = FindEnabledEditorScenes();
+
+        static string APP_NAME = "Elements";
+        static string TARGET_DIR = "C:/Users/Aaron/Desktop";
+
+        [MenuItem ("Custom/Build Android")]
+        static void PerformMacOSXBuild ()
+        {
+                 string target_dir = APP_NAME + ".apk";
+                 GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.Android() {
+		List<string> EditorScenes = new List<string>();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+
+        static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+        {
+                EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+                string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
+                if (res.Length > 0) {
+                        throw new Exception("BuildPlayer failure: " + res);
+                }
+        }
+}
