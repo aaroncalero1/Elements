d2b27ad43eb5956a1f31c90a9e1a965669baf775
gontzalaparicio <laztnog@hotmail.com>
2014-05-08 16:08:46 +0200

    script bat

---
+++ script.bat
@@ -0,0 +1,2 @@
+
+"C:\Program Files (x86)\Unity\Editor\Unity.exe" -quit -batchmode -executeMethod MyEditorScript.PerformAndroidBuild -nographics -projectPath "C:\Users\Gontzal\Documents\ElementsEvolve"
