53046be5edc058f2fa2da75189f3eb97e67e8096
gontzalaparicio <laztnog@hotmail.com>
2014-05-08 16:26:03 +0200

    adjillo

--- script.bat
+++ script.bat
@@ -1,2 +1,3 @@
-
-"C:\Program Files (x86)\Unity\Editor\Unity.exe" -quit -batchmode -executeMethod MyEditorScript.PerformAndroidBuild -nographics -projectPath "C:\Users\Gontzal\Documents\ElementsEvolve"
+cd "C:\Users\Gontzal\Documents\ElementsEvolveForPull"
+git pull https://github.com/aaroncalero1/Elements.git
+"C:\Program Files (x86)\Unity\Editor\Unity.exe" -quit -batchmode -executeMethod MyEditorScript.PerformAndroidBuild -nographics -projectPath "C:\Users\Gontzal\Documents\ElementsEvolveForPull"
